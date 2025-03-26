fa925d11dac65ab421ca60ea1023ed758e870414
Kornel-crypto <novakoka66@gmail.com>
2025-03-26 05:54:02 +0100

    Kornel-crypto
    change tailwind expression into apply form to refoctoring the previously management

--- src/app/explorer/page.js
+++ src/app/explorer/page.js
@@ -1,13 +1,12 @@
 export default function ExplorePage() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">
-        Explorer les lieux de tournage
-      </h1>
-      <p>
+    <div className="conteneur-principal">
+      <h1 className="titre-section">Explorer les lieux de tournage</h1>
+      <p className="paragraphe">
         Cette page est destinée pour la recherche de tournage depuis un fichier
         csv ou une base de donnée
       </p>
+      <button className="bouton-secondaire">En savoir plus</button>
     </div>
   );
 }
